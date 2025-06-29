31c4c2a7dfad545c42932a02d44e1e033b2065be
LuxeLaps <onandu978@gmail.com>
2025-06-30 00:14:01 +0530

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,23 @@
 function Navbar() {
     return (
-        <nav className="nav bg-gray-800 text-white">
-            <ul className="flex items-center justify-between p-4">
-                {/* Logo and Portfolio */}
+        <nav className="bg-gray-800 text-white">
+            <ul className="flex items-center justify-between p-4 w-full">
+                {/* Left section: Logo and title */}
                 <div className="flex items-center space-x-2">
-                    {/* Replace the src with your logo path */}
                     <img src="/logo.png" alt="Logo" className="h-8 w-8" />
                     <span className="text-lg font-bold">Portfolio</span>
-                
-                {/* Navigation Links */}
+                </div>
 
-                    <li className="flex space-x-6">
+                {/* Right section: Navigation links */}
+                <li className="flex space-x-6 list-none">
                     <a href="/" className="hover:text-gray-400">Home</a>
                     <a href="#about" className="hover:text-gray-400">About</a>
                     <a href="#services" className="hover:text-gray-400">Services</a>
                     <a href="#projects" className="hover:text-gray-400">Projects</a>
-                    </li>
-                </div>
+                </li>
             </ul>
         </nav>
     );
 }
-export default Navbar
+
+export default Navbar;
